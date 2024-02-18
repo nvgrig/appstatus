ea5de10f888263698073d7a8aca36ee81c43b55c
Nikita Grigoryev <nvgrigoryev@gmail.com>
2024-02-18 10:30:17 +0400

    add method to make DataDto from ProcessData

--- src/main/java/com/nvgrig/appstatus/mapper/Mapper.java
+++ src/main/java/com/nvgrig/appstatus/mapper/Mapper.java
@@ -1,9 +1,9 @@
 package com.nvgrig.appstatus.mapper;
 
-import com.nvgrig.appstatus.dto.DataModelDto;
-import com.nvgrig.appstatus.model.DataModel;
+import com.nvgrig.appstatus.dto.DataDto;
+import com.nvgrig.appstatus.model.ProcessedData;
 
 public interface Mapper {
 
-    DataModelDto from(DataModel dataModel);
+    DataDto from(ProcessedData processedData);
 }
