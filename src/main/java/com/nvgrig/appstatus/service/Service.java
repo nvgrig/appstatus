8bcc2bee801a1f2bdce2c2065ee5513796d6d1a0
Nikita Grigoryev <nvgrigoryev@gmail.com>
2024-02-18 10:29:07 +0400

    add processData method

--- src/main/java/com/nvgrig/appstatus/service/Service.java
+++ src/main/java/com/nvgrig/appstatus/service/Service.java
@@ -1,8 +1,11 @@
 package com.nvgrig.appstatus.service;
 
 import com.nvgrig.appstatus.model.DataModel;
+import com.nvgrig.appstatus.model.ProcessedData;
 
 public interface Service {
 
     DataModel getData();
+
+    ProcessedData processData(DataModel dataModel);
 }
